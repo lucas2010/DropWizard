1345433b7e8d09163a80812da2017509e397b44e
Luis Caselli <lucas2010>
2019-06-10 09:18:12 +0100

    First Commit

---
+++ src/main/java/pt/lucas2010/dropwizardRestfulTest2Configuration.java
@@ -0,0 +1,10 @@
+package pt.lucas2010;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class dropwizardRestfulTest2Configuration extends Configuration {
+    // TODO: implement service configuration
+}
